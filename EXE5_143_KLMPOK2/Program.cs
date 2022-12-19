8c9621f92fe8ff373407be573542a46bc746aaa5
overol1221 <fiqiachmad12@gmail.com>
2022-12-19 15:43:06 +0700

    Add project files.

---
+++ EXE5_143_KLMPOK2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_143_KLMPOK2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
