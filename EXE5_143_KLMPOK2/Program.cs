125709a891c238b1217f0a02c469b2a9fba8489a
overol1221 <fiqiachmad12@gmail.com>
2022-12-19 16:35:17 +0700

    Create class node and add make a variable

--- EXE5_143_KLMPOK2/Program.cs
+++ EXE5_143_KLMPOK2/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace EXE5_143_KLMPOK2
 {
-    internal class Program
+
+    class node
     {
-        static void Main(string[] args)
+        int AHMAD, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+        public node()
         {
+            /*intializing the values of the variables REAR and FRONT to -1 to indicate that * the queue is intializing empty.*/
+            AHMAD = -1;
+            REAR = -1;
         }
     }
 }
