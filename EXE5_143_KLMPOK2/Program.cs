b629288ac07a37f3773db54669fab5864b9a51bf
overol1221 <fiqiachmad12@gmail.com>
2022-12-19 16:40:34 +0700

    Create rear for last position

--- EXE5_143_KLMPOK2/Program.cs
+++ EXE5_143_KLMPOK2/Program.cs
@@ -30,6 +30,17 @@ namespace EXE5_143_KLMPOK2
                 AHMAD = 0;
                 REAR = 0;
             }
+            else
+            {
+               
+                if (REAR == max - 1)
+                    REAR = 0;
+                else;
+                
+                REAR = REAR + 1;
+            }
+            queue_array[REAR] = element;
         }
+
     }
-}
+    }
